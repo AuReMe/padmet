37d5816addd882715e5b3900f7cc5d5a8b031471
mezianeAITE <meziane.aite@inria.fr>
2019-02-08 14:54:49 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.mathjax", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "nbsphinx"]
+extensions = ["sphinx.ext.mathjax", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 
 # Add any paths that contain templates here, relative to this directory.
