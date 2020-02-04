2e96749a754ac756f284419c5a01f85cab18ef85
Arnaud Belcour <arnaud.belcour@irisa.fr>
2020-02-04 11:37:26 +0100

    Move test from sbmlPlugin to test folder.

--- padmet/utils/sbmlPlugin.py
+++ padmet/utils/sbmlPlugin.py
@@ -297,28 +297,3 @@ def get_all_decoded_version(element_id, _type):
             )
     return all_element_id_decoded
 
-
-def test():
-    # test convert_from_coded_id
-    coded = "R_RXN__45__11921"
-    assert convert_from_coded_id(coded) == ("RXN-11921", "R", None)
-
-    coded = "R_R00332_c"
-    assert convert_from_coded_id(coded) == ("R00332", "R", "c")
-
-    coded = "S_N6_45__40_L_45_1_44_3_45_Dicarboxypropyl_41__45_L_45_lysine_c"
-    assert convert_from_coded_id(coded, pattern="_", species_tag="S") == (
-        "N6-(L-1,3-Dicarboxypropyl)-L-lysine",
-        "S",
-        "c",
-    )
-
-    coded = "S__40_2R_41__45_2_45_Hydroxy_45_3_45__40_phosphonooxy_41__45_propanal_c"
-    assert convert_from_coded_id(coded, pattern="_", species_tag="S") == (
-        "(2R)-2-Hydroxy-3-(phosphonooxy)-propanal",
-        "S",
-        "c",
-    )
-
-    coded = "M_citr_L_m"
-    assert convert_from_coded_id(coded) == ("citr_L", "M", "m")
---
+++ tests/test_sbmlPlugin.py
@@ -0,0 +1,27 @@
+
+from padmet.utils import sbmlPlugin
+
+def test_sbmlPlugin():
+    # test convert_from_coded_id
+    coded = "R_RXN__45__11921"
+    assert sbmlPlugin.convert_from_coded_id(coded) == ("RXN-11921", "R", None)
+
+    coded = "R_R00332_c"
+    assert sbmlPlugin.convert_from_coded_id(coded) == ("R00332", "R", "c")
+
+    coded = "S_N6_45__40_L_45_1_44_3_45_Dicarboxypropyl_41__45_L_45_lysine_c"
+    assert sbmlPlugin.convert_from_coded_id(coded, pattern="_", species_tag="S") == (
+        "N6-(L-1,3-Dicarboxypropyl)-L-lysine",
+        "S",
+        "c",
+    )
+
+    coded = "S__40_2R_41__45_2_45_Hydroxy_45_3_45__40_phosphonooxy_41__45_propanal_c"
+    assert sbmlPlugin.convert_from_coded_id(coded, pattern="_", species_tag="S") == (
+        "(2R)-2-Hydroxy-3-(phosphonooxy)-propanal",
+        "S",
+        "c",
+    )
+
+    coded = "M_citr_L_m"
+    assert sbmlPlugin.convert_from_coded_id(coded) == ("citr_L", "M", "m")
