2b47bb9a19d3b6f9a1dae541ce572cb0f9078316
Arnaud Belcour <arnaud.belcour@irisa.fr>
2020-02-27 18:05:10 +0100

    Add creation of newick for metacyc ontology.

--- padmet/utils/connection/get_metacyc_ontology.py
+++ padmet/utils/connection/get_metacyc_ontology.py
@@ -8,7 +8,7 @@ Description:
 from lxml import etree
 from padmet.classes.padmetRef import PadmetRef
 
-def metacyc_to_ontology(padmetRef_file, output_file):
+def metacyc_to_ontology(padmetRef_file, output_file, ontology_root='FRAMES'):
     """
     Extract teh ontology of MetaCyc from the padmetRef.
 
@@ -18,6 +18,8 @@ def metacyc_to_ontology(padmetRef_file, output_file):
         path to padmetRef file
     output_file: str
         pathname of the output sbml
+    ontology_root: str
+        name of the roots to use to create the tree (FRAMES, Generalized-Reactions, Compounds, ...)
     """
     padmetref = PadmetRef(padmetRef_file)
 
@@ -25,29 +27,103 @@ def metacyc_to_ontology(padmetRef_file, output_file):
 
     known_parents = {}
 
-    # FRAMES can be replaced wiht Generalized-Reactions (for reactions/pathways) or Compounds
-    frames = etree.Element("FRAMES")
+    # Create the root of the xml tree.
+    frames = etree.Element("element_" + str(len(known_parents)), name=ontology_root)
 
-    known_parents['FRAMES'] = frames
+    known_parents[ontology_root] = frames
 
+    # Extract the parent of each objects from classes.
     child_parents = {}
-
     for class_node in class_nodes:
         if class_node.id != 'FRAMES':
             parent_classes = [rlt.id_out for rlt in padmetref.dicOfRelationIn[class_node.id] if rlt.type == 'is_a_class']
             child_parents[class_node.id] = parent_classes
 
     def get_child(parent_id, known_parents, child_parents):
+        # Search all the child of the parent_id
         for child_parent in child_parents:
             for parent_class in child_parents[child_parent]:
                 if parent_class == parent_id:
                     if parent_class in known_parents:
-                        et_subelement = etree.SubElement(known_parents[parent_class], 'ont_'+child_parent.replace('+','').replace(':',''))
+                        et_subelement = etree.SubElement(known_parents[parent_class], "element_" + str(len(known_parents)), name=child_parent)
                         known_parents[child_parent] = et_subelement
                         get_child(child_parent, known_parents, child_parents)
 
 
-    get_child('FRAMES', known_parents, child_parents)
+    get_child(ontology_root, known_parents, child_parents)
 
     tree = etree.ElementTree(frames)
     tree.write(output_file, pretty_print=True)
+
+
+def extract_pwy_ontology(metacyc_ontology_file, padmetRef_file, output_file):
+    onttree = etree.parse(metacyc_ontology_file)
+    ontology_elements = {}
+    for element in onttree.iter():
+        if element.attrib['name'] not in ontology_elements:
+            ontology_elements[element.attrib['name']] = [element]
+        else:
+            ontology_elements[element.attrib['name']].append(element)
+
+    padmetref = PadmetRef(padmetRef_file)
+
+    pwys_id = [node.id for node in padmetref.dicOfNode.values() if node.type == "pathway"]
+
+    count = 0
+    for pathway_id in pwys_id:
+        pathway_classes = [rlt.id_out for rlt in padmetref.dicOfRelationIn[pathway_id] if rlt.type == "is_a_class"]
+        for pathway_class in pathway_classes:
+            for subclass in ontology_elements[pathway_class]:
+                etree.SubElement(subclass, 'pathway_element_' + str(count), name=pathway_id)
+                count += 1
+
+    tree = etree.ElementTree(onttree.getroot())
+    tree.write(output_file, pretty_print=True)
+
+
+def extract_compound_ontology(metacyc_ontology_file, padmetRef_file, output_file):
+    onttree = etree.parse(metacyc_ontology_file)
+    ontology_elements = {}
+    for element in onttree.iter():
+        if element.attrib['name'] not in ontology_elements:
+            ontology_elements[element.attrib['name']] = [element]
+        else:
+            ontology_elements[element.attrib['name']].append(element)
+
+    padmetref = PadmetRef(padmetRef_file)
+
+    compound_ids = [node.id for node in padmetref.dicOfNode.values() if node.type == "compound"]
+
+    count = 0
+    for compound_id in compound_ids:
+        if compound_id in padmetref.dicOfRelationIn:
+            compound_classes = [rlt.id_out for rlt in padmetref.dicOfRelationIn[compound_id] if rlt.type == "is_a_class"]
+            for compound_classe in compound_classes:
+                for subclass in ontology_elements[compound_classe]:
+                    etree.SubElement(subclass, 'compound_element_' + str(count), name=compound_id)
+                    count += 1
+
+    tree = etree.ElementTree(onttree.getroot())
+    tree.write(output_file, pretty_print=True)
+
+
+def ontology_to_newick(metacyc_ontology_file, newick_output_file):
+    onttree = etree.parse(metacyc_ontology_file)
+
+    def child_to_newick(node):
+        childrens = []
+        if node.getparent() is None:
+            childrens.append(node.attrib['name'])
+        for children in node.getchildren():
+            if len(children.getchildren()) > 0:
+                subchilds = []
+                childs = child_to_newick(children)
+                subchilds.append("'"+children.attrib['name']+"'")
+                subchilds.append(childs)
+                childrens.append('(' + ','.join(subchilds) + ')')
+            else:
+                childrens.append("'"+children.attrib['name']+"'")
+        return '(' + ','.join(childrens) + ')'
+
+    with open(newick_output_file, 'w') as newick:
+        newick.write(child_to_newick(onttree.getroot())+';')
