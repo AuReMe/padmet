d0dd658a4530eb2b0bcb6aa22352a035719f0727
Arnaud Belcour <arnaud.belcour@gmail.com>
2020-04-01 18:00:27 +0200

    Add multiprocessing in compare_padmet.

--- padmet/utils/exploration/compare_padmet.py
+++ padmet/utils/exploration/compare_padmet.py
@@ -16,11 +16,130 @@ Description:
         line = [cpd-1, rxn-1,'',rxn-1,'']
 
 """
-from padmet.classes import PadmetSpec
-import os
 import csv
+import os
 
-def compare_padmet(padmet_path, output, padmetRef = None, verbose = False):
+from multiprocessing import Pool
+from padmet.classes import PadmetSpec
+
+
+def extract_information_padmet(file_path, padmetRef, verbose):
+    count_elem, dict_genes, dict_rxns, dict_pwys, dict_cpds = {}, {}, {}, {}, {}
+    #cout_elem: k = basename_file, v = dict(k' in  [gene,rxn,pwy,metabolite], v = count element)
+    #ex: count_elemn = {'file_1':{'genes':1500,'rxn':1000,pathways:1000,'metabolites':1000}}
+    #dict_genes: k = gene_id, v = dict(k' = basename_file, v = set of rxn assoc)
+    #ex: dict_genes = {'gene_a': {'file_1': set([rxn_1,rxn_2])}}
+    #dict_rxns: k = rxn_id, v = dict(k' = basename_file, v' = dict(k'' in ['genes_assoc','formula'])
+    #ex: dict_rxns = {'rxn_a': {'file_1': {'genes_assoc': set([gene_a,gene_b]), 'formula':'1x + 2y => 3z', 'source_categories':'', source_tools, sources:''}}
+    #dict_pwys: k = pwy_id, , v = dict(k' = basename_file, v' = dict(k'' in ['ratio','rxn_assoc'])
+    #ex: dict_pwys = {'pwy_a': {'file_1': {'ratio': 2/9, 'rxn_assoc':'rxn_a;rxn_b'}}}
+    #dict_cpds
+    padmet = PadmetSpec(file_path)
+    basename_file = os.path.basename(file_path).replace(".padmet","")
+    if verbose: print("reading %s" %basename_file)
+    count_elem[basename_file] = {"genes":0,"reactions":0,"pathways":0,"metabolites":0}
+
+    #genes
+    all_genes = set([node.id for node in list(padmet.dicOfNode.values()) if node.type == "gene"])
+    count_elem[basename_file]["genes"] = len(all_genes)
+    if verbose: print("\t%s genes..." %len(all_genes))
+    for gene_id in all_genes:
+        #get for each gene, the reactiosn associated to and add in dict_gene: dict_gene[gene_id] = {padmet_file_name: set of rxn assoc}
+        rxn_assoc = set([rlt.id_in for rlt in padmet.dicOfRelationOut.get(gene_id,[]) if rlt.type == "is_linked_to"])
+        try:
+            dict_genes[gene_id][basename_file] = ";".join(rxn_assoc)
+        except KeyError:
+            dict_genes[gene_id] = {basename_file: ";".join(rxn_assoc)}
+
+    #reactions
+    all_rxns = set([node.id for node in list(padmet.dicOfNode.values()) if node.type == "reaction"])
+    count_elem[basename_file]["reactions"] = len(all_rxns)
+    if verbose: print("\t%s reactions..." %len(all_rxns))
+    for rxn_id in all_rxns:
+        try:
+            dict_rxns[rxn_id][basename_file] = {"genes_associated":"", "formula":""}
+        except KeyError:
+            dict_rxns[rxn_id] = {basename_file: {"genes_associated":"", "formula":""}}
+
+        #get for each rxn, genes associated to and add in dict_rxn: dict_rxns[rxn_id] = {padmet_file_name: {'genes_associations':set of rxn assoc}}
+        genes_assoc = set([rlt.id_out for rlt in padmet.dicOfRelationIn.get(rxn_id,[]) if rlt.type == "is_linked_to"])
+        dict_rxns[rxn_id][basename_file]["genes_associated"] = ";".join(genes_assoc)
+        #create formula
+        direction = padmet.dicOfNode[rxn_id].misc["DIRECTION"][0]
+        if direction == "UNKNOWN":
+            direction = " =>/<=> "
+        elif direction == "REVERSIBLE":
+            direction = " <=> "
+        elif direction == "LEFT-TO-RIGHT":
+            direction = " => "
+        #reactants and products: list of str: ['stoichio [[cpd_id]][cpd_compart]',...]
+        reactants = [rlt.misc["STOICHIOMETRY"][0]+" "+rlt.id_out+"["+rlt.misc["COMPARTMENT"][0]+"]"
+        for rlt in padmet.dicOfRelationIn.get(rxn_id, None) if rlt.type == "consumes"]
+        products = [rlt.misc["STOICHIOMETRY"][0]+" "+rlt.id_out+"["+rlt.misc["COMPARTMENT"][0]+"]"
+        for rlt in padmet.dicOfRelationIn.get(rxn_id, None) if rlt.type == "produces"]
+        #join each list with '+' and direction in center
+        formula = " + ".join(reactants)+direction+" + ".join(products)
+        dict_rxns[rxn_id][basename_file]["formula"] = formula
+
+    #pathways
+    all_pwys = set()
+    for rxn_id in all_rxns:
+        pwy_in = set([rlt.id_out for rlt in padmet.dicOfRelationIn[rxn_id] if rlt.type == "is_in_pathway"])
+        all_pwys.update(pwy_in)
+    count_elem[basename_file]["pathways"] = len(all_pwys)
+    if verbose: print("\t%s pathways..." %len(all_pwys))
+    for pwy_id in all_pwys:
+        #get for each pwy, the reactions associated to and add in dict_gene: dict_gene[gene_id] = {padmet_file_name: set of rxn assoc}
+        try:
+            dict_pwys[pwy_id][basename_file] = {"ratio":"", "rxn_associated":""}
+        except KeyError:
+            dict_pwys[pwy_id] = {basename_file: {"ratio":"", "rxn_associated":""}}
+        in_rxns = set([rlt.id_in for rlt in padmet.dicOfRelationOut.get(pwy_id,[]) if rlt.type == "is_in_pathway"])
+        if padmetRef:
+            all_rxns = set([rlt.id_in for rlt in padmetRef.dicOfRelationOut.get(pwy_id,[]) if rlt.type == "is_in_pathway"])
+            dict_pwys[pwy_id][basename_file]["ratio"] = str(len(in_rxns))+"/"+str(len(all_rxns))
+        else:
+            dict_pwys[pwy_id][basename_file]["ratio"] = "NA"
+        dict_pwys[pwy_id][basename_file]["rxn_associated"] = ";".join(in_rxns)
+
+    #metabolites
+    all_cpd = set([rlt.id_out for rlt in padmet.getAllRelation() if rlt.type in ["consumes","produces"]])
+    count_elem[basename_file]["metabolites"] = len(all_cpd)
+    if verbose: print("\t%s metabolites..." %len(all_cpd))
+    for cpd_id in all_cpd:
+        #get for each cpd, the reactions consuming/producing the cpd
+        rxn_consume = set([rlt.id_in for rlt in padmet.dicOfRelationOut[cpd_id] if rlt.type == "consumes"])
+        if rxn_consume:
+            rxn_consume = ";".join(rxn_consume)
+        else:
+            rxn_consume = ""
+        rxn_produce = set([rlt.id_in for rlt in padmet.dicOfRelationOut[cpd_id] if rlt.type == "produces"])
+        if rxn_produce:
+            rxn_produce = ";".join(rxn_produce)
+        else:
+            rxn_produce = ""
+        try:
+            dict_cpds[cpd_id][basename_file] = {"rxn_consume":rxn_consume, "rxn_produce":rxn_produce}
+        except KeyError:
+            dict_cpds[cpd_id] = {basename_file: {"rxn_consume":rxn_consume, "rxn_produce":rxn_produce}}
+
+    return dict_genes, dict_rxns, dict_pwys, dict_cpds
+
+
+def merge_dicts(element_dict, tmp_dict):
+    for gene_id in tmp_dict:
+        if gene_id in element_dict:
+            for basename_file in tmp_dict[gene_id]:
+                element_dict[gene_id][basename_file] = tmp_dict[gene_id][basename_file]
+        else:
+            element_dict[gene_id] = {}
+            for basename_file in tmp_dict[gene_id]:
+                element_dict[gene_id][basename_file] = tmp_dict[gene_id][basename_file]
+
+    return element_dict
+
+
+def compare_padmet(padmet_path, output, padmetRef = None, verbose = False, number_cpu = None):
     """
     #Compare 1-n padmet and create a folder output with files:
     genes.csv:
@@ -48,6 +167,7 @@ def compare_padmet(padmet_path, output, padmetRef = None, verbose = False):
         if True print information
     
     """
+    dict_genes, dict_rxns, dict_pwys, dict_cpds = {}, {}, {}, {}
     if not os.path.exists(output):
         if verbose: print("Creating %s" %output)
         os.makedirs(output)
@@ -65,104 +185,33 @@ def compare_padmet(padmet_path, output, padmetRef = None, verbose = False):
         print(("%s padmet files to compare:" %len(all_files)))
         for f in all_files:
             print("\t%s" %os.path.basename(f))
-    count_elem, dict_genes, dict_rxns, dict_pwys, dict_cpds = {}, {}, {}, {}, {}
-    #cout_elem: k = basename_file, v = dict(k' in  [gene,rxn,pwy,metabolite], v = count element)
-    #ex: count_elemn = {'file_1':{'genes':1500,'rxn':1000,pathways:1000,'metabolites':1000}}
-    #dict_genes: k = gene_id, v = dict(k' = basename_file, v = set of rxn assoc)
-    #ex: dict_genes = {'gene_a': {'file_1': set([rxn_1,rxn_2])}} 
-    #dict_rxns: k = rxn_id, v = dict(k' = basename_file, v' = dict(k'' in ['genes_assoc','formula'])
-    #ex: dict_rxns = {'rxn_a': {'file_1': {'genes_assoc': set([gene_a,gene_b]), 'formula':'1x + 2y => 3z', 'source_categories':'', source_tools, sources:''}}
-    #dict_pwys: k = pwy_id, , v = dict(k' = basename_file, v' = dict(k'' in ['ratio','rxn_assoc'])
-    #ex: dict_pwys = {'pwy_a': {'file_1': {'ratio': 2/9, 'rxn_assoc':'rxn_a;rxn_b'}}}
-    #dict_cpds
-    for file_path in all_files:
-        padmet = PadmetSpec(file_path)
-        basename_file = os.path.basename(file_path).replace(".padmet","")
-        if verbose: print("reading %s" %basename_file)
-        count_elem[basename_file] = {"genes":0,"reactions":0,"pathways":0,"metabolites":0}
-        #genes
-        all_genes = set([node.id for node in list(padmet.dicOfNode.values()) if node.type == "gene"])
-        count_elem[basename_file]["genes"] = len(all_genes)
-        if verbose: print("\t%s genes..." %len(all_genes))
-        for gene_id in all_genes:
-            #get for each gene, the reactiosn associated to and add in dict_gene: dict_gene[gene_id] = {padmet_file_name: set of rxn assoc}
-            rxn_assoc = set([rlt.id_in for rlt in padmet.dicOfRelationOut.get(gene_id,[]) if rlt.type == "is_linked_to"])
-            try:
-                dict_genes[gene_id][basename_file] = ";".join(rxn_assoc)
-            except KeyError:
-                dict_genes[gene_id] = {basename_file: ";".join(rxn_assoc)}
-        
-        #reactions
-        all_rxns = set([node.id for node in list(padmet.dicOfNode.values()) if node.type == "reaction"])
-        count_elem[basename_file]["reactions"] = len(all_rxns)
-        if verbose: print("\t%s reactions..." %len(all_rxns))
-        for rxn_id in all_rxns:
-            try:
-                dict_rxns[rxn_id][basename_file] = {"genes_associated":"", "formula":""}
-            except KeyError:
-                dict_rxns[rxn_id] = {basename_file: {"genes_associated":"", "formula":""}}
 
-            #get for each rxn, genes associated to and add in dict_rxn: dict_rxns[rxn_id] = {padmet_file_name: {'genes_associations':set of rxn assoc}}
-            genes_assoc = set([rlt.id_out for rlt in padmet.dicOfRelationIn.get(rxn_id,[]) if rlt.type == "is_linked_to"])
-            dict_rxns[rxn_id][basename_file]["genes_associated"] = ";".join(genes_assoc)
-            #create formula
-            direction = padmet.dicOfNode[rxn_id].misc["DIRECTION"][0]
-            if direction == "UNKNOWN":
-                direction = " =>/<=> "
-            elif direction == "REVERSIBLE":
-                direction = " <=> "
-            elif direction == "LEFT-TO-RIGHT":
-                direction = " => "
-            #reactants and products: list of str: ['stoichio [[cpd_id]][cpd_compart]',...]
-            reactants = [rlt.misc["STOICHIOMETRY"][0]+" "+rlt.id_out+"["+rlt.misc["COMPARTMENT"][0]+"]"
-            for rlt in padmet.dicOfRelationIn.get(rxn_id, None) if rlt.type == "consumes"]
-            products = [rlt.misc["STOICHIOMETRY"][0]+" "+rlt.id_out+"["+rlt.misc["COMPARTMENT"][0]+"]"
-            for rlt in padmet.dicOfRelationIn.get(rxn_id, None) if rlt.type == "produces"]
-            #join each list with '+' and direction in center
-            formula = " + ".join(reactants)+direction+" + ".join(products)
-            dict_rxns[rxn_id][basename_file]["formula"] = formula
+    if number_cpu:
+        try:
+            number_cpu_to_use = int(number_cpu)
+        except ValueError:
+            raise ValueError('The number of CPU must be an integer.')
+    else:
+        number_cpu_to_use = 1
+    compare_pool = Pool(processes=number_cpu_to_use)
 
-        #pathways
-        all_pwys = set()
-        for rxn_id in all_rxns:
-            pwy_in = set([rlt.id_out for rlt in padmet.dicOfRelationIn[rxn_id] if rlt.type == "is_in_pathway"])
-            all_pwys.update(pwy_in)
-        count_elem[basename_file]["pathways"] = len(all_pwys)
-        if verbose: print("\t%s pathways..." %len(all_pwys))
-        for pwy_id in all_pwys:
-            #get for each pwy, the reactions associated to and add in dict_gene: dict_gene[gene_id] = {padmet_file_name: set of rxn assoc}
-            try:
-                dict_pwys[pwy_id][basename_file] = {"ratio":"", "rxn_associated":""}
-            except KeyError:
-                dict_pwys[pwy_id] = {basename_file: {"ratio":"", "rxn_associated":""}}
-            in_rxns = set([rlt.id_in for rlt in padmet.dicOfRelationOut.get(pwy_id,[]) if rlt.type == "is_in_pathway"])
-            if padmetRef:
-                all_rxns = set([rlt.id_in for rlt in padmetRef.dicOfRelationOut.get(pwy_id,[]) if rlt.type == "is_in_pathway"])
-                dict_pwys[pwy_id][basename_file]["ratio"] = str(len(in_rxns))+"/"+str(len(all_rxns))
-            else:
-                dict_pwys[pwy_id][basename_file]["ratio"] = "NA"
-            dict_pwys[pwy_id][basename_file]["rxn_associated"] = ";".join(in_rxns)
-        
-        #metabolites
-        all_cpd = set([rlt.id_out for rlt in padmet.getAllRelation() if rlt.type in ["consumes","produces"]])
-        count_elem[basename_file]["metabolites"] = len(all_cpd)
-        if verbose: print("\t%s metabolites..." %len(all_cpd))
-        for cpd_id in all_cpd:
-            #get for each cpd, the reactions consuming/producing the cpd
-            rxn_consume = set([rlt.id_in for rlt in padmet.dicOfRelationOut[cpd_id] if rlt.type == "consumes"])
-            if rxn_consume:
-                rxn_consume = ";".join(rxn_consume)
-            else:
-                rxn_consume = ""
-            rxn_produce = set([rlt.id_in for rlt in padmet.dicOfRelationOut[cpd_id] if rlt.type == "produces"])
-            if rxn_produce:
-                rxn_produce = ";".join(rxn_produce)
-            else:
-                rxn_produce = ""
-            try:
-                dict_cpds[cpd_id][basename_file] = {"rxn_consume":rxn_consume, "rxn_produce":rxn_produce}
-            except KeyError:
-                dict_cpds[cpd_id] = {basename_file: {"rxn_consume":rxn_consume, "rxn_produce":rxn_produce}}
+    multiprocessin_datas = []
+    for padmet_file in all_files:
+        multiprocessin_datas.append([padmet_file, padmetRef, verbose])
+    result_rxns_dicts = compare_pool.starmap(extract_information_padmet, multiprocessin_datas)
+
+    for result_rxns_dict in result_rxns_dicts:
+        tmp_dict_genes = result_rxns_dict[0]
+        tmp_dict_rxns = result_rxns_dict[1]
+        tmp_dict_pwys = result_rxns_dict[2]
+        tmp_dict_cpds = result_rxns_dict[3]
+        dict_genes = merge_dicts(dict_genes, tmp_dict_genes)
+        dict_rxns = merge_dicts(dict_rxns, tmp_dict_rxns)
+        dict_pwys = merge_dicts(dict_pwys, tmp_dict_pwys)
+        dict_cpds = merge_dicts(dict_cpds, tmp_dict_cpds)
+
+    compare_pool.close()
+    compare_pool.join()
 
     #create files
     all_basename_files = [os.path.basename(file_path).replace(".padmet","") for file_path in all_files]
