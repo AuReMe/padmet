74c399b24051d475b2ed77aeeafafa768458c78e
Jeanne GOT <44268849+jeannegot@users.noreply.github.com>
2023-12-19 13:03:34 +0100

    Update wikiGenerator.py

--- padmet/utils/connection/wikiGenerator.py
+++ padmet/utils/connection/wikiGenerator.py
@@ -379,8 +379,8 @@ def extract_padmet_data(padmetFile, total_padmet_data, global_pwy_rxn_dict=None,
                         assignment = "n.a"
                     try:
                         rec_data_node = next(node for node in list(padmet.dicOfNode.values()) if (node.type == "reconstructionData" and node.misc.get("SOURCE",["unknown-source"])[0] == source))
-                        except StopIteration:
-                            rec_data_node = list(padmet.dicOfNode.values())[-1]
+                    except StopIteration:
+                        rec_data_node = list(padmet.dicOfNode.values())[-1]
                     category = rec_data_node.misc.get("CATEGORY",["unknown-category"])[0].lower()
                     tool = rec_data_node.misc.get("TOOL",["unknown-tool"])[0].lower()
                     comment = rec_data_node.misc.get("COMMENT",["n.a"])[0].lower()
